d7ee57670282916b5c57715fd2ac9558660019b3
17034021 <17034021@rp.edu.sg>
2019-05-14 18:15:39 +0800

    Initial commit

---
+++ app/src/main/java/sg/edu/rp/c346/timeanddateapp/MainActivity.java
@@ -0,0 +1,68 @@
+package sg.edu.rp.c346.timeanddateapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.TextView;
+import android.widget.TimePicker;
+
+public class MainActivity extends AppCompatActivity {
+
+    DatePicker dp;
+    TimePicker tp;
+    Button btnDisplayDate;
+    Button btnDisplayTime;
+    TextView tvDisplay;
+    Button btnReset;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        dp = findViewById(R.id.datePicker);
+        tp = findViewById(R.id.timePicker);
+        btnDisplayDate = findViewById(R.id.buttonDisplayDate);
+        btnDisplayTime = findViewById(R.id.buttonDisplayTime);
+        tvDisplay = findViewById(R.id.textView);
+        btnReset = findViewById(R.id.resetButton);
+
+        btnDisplayTime.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                tvDisplay.setText("Time is " + tp.getCurrentHour() + ":" + tp.getCurrentMinute());
+            }
+        });
+
+        btnDisplayDate.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                int day = dp.getDayOfMonth();
+                int month = dp.getMonth() + 1;
+                int year = dp.getYear();
+                String response1 = "The date is " + day + "/" + month + "/" + year;
+                tvDisplay.setText(response1);
+
+            }
+        });
+
+        btnReset.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+                tp.setCurrentHour(0);
+                tp.setCurrentMinute(0);
+                dp.updateDate(2019, 0 , 01);
+            }
+        });
+    }
+}
+
+
+
+
+
+
+
